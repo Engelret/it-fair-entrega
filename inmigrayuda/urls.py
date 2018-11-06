f08b34ad8cf26cf3d4476756f75172adab2c7c2c
Engel <eng.retamal@alumnos.duoc.cl>
2018-11-06 12:12:17 -0300

    admin
    creacion admin, creacion vista admin, creacion formularios de actualizacion admin

--- inmigrayuda/urls.py
+++ inmigrayuda/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,4 +11,7 @@ urlpatterns = [
     path('contacto/',views.contacto,name="contacto"),
     path('noticias/',views.noticias,name="noticias"),
     path('categorias/',views.categorias,name="categorias"),
+    path('login',views.login,name="login"),
+    path('cerrarsession',views.cerrar_session,name="cerrar_session"),
+    path('login/iniciar',views.login_iniciar,name="iniciar")
 ]
--- inmigrayuda/views.py
+++ inmigrayuda/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
+#importar user
+from django.contrib.auth.models import User
+#sistema de autenticación 
+from django.contrib.auth import authenticate,logout, login as auth_login
+
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
-    return render(request,'index.html',{})
+    usuario = request.session.get('usuario',None)
+    return render(request,'index.html',{'usuario':usuario})
 
 def preguntasFrecuentes(request):
-    return render(request,'preguntasFrecuentes.html',{})
+    usuario = request.session.get('usuario',None)
+    return render(request,'preguntasFrecuentes.html',{'usuario':usuario})
 
 def contacto(request):
     return render(request,'contacto.html',{})
@@ -16,7 +25,29 @@ def quienesSomos(request):
     return render(request,'quienesSomos.html',{})
 
 def noticias(request):
-    return render(request,'noticias.html',{})
+    usuario = request.session.get('usuario',None)
+    return render(request,'noticias.html',{'usuario':usuario})
 
 def categorias(request):
     return render(request,'categorias.html',{})
+
+def login(request):
+    return render(request,'login.html',{})
+
+def login_iniciar(request):
+    usuario = request.POST.get('nombre_usuario','')
+    contrasenia = request.POST.get('contrasenia','')
+    user = authenticate(request,username=usuario, password=contrasenia)
+
+    if user is not None:
+        auth_login(request, user)
+        request.session['usuario'] = user.first_name+" "+user.last_name
+        return redirect("index")
+    else:
+        return redirect("login")
+
+
+@login_required(login_url='login')
+def cerrar_session(request):
+    logout(request)
+    return redirect('index')
